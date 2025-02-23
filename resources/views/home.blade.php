f39b4093c445ae736e6e4a4b14a7841153baf5a9
Benjamin Payne <ben@Bens-Mac-Mini-2.local>
2025-02-23 14:20:59 -0500

    dev commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -869,17 +869,17 @@
                                 <div class="sm:col-span-2 relative">
                                     <label for="notes" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Notes</label>
 
-                                    @if (isset($carrier) && ! $carrier->carrierNotes->isEmpty())
-                                        <ol id="carrier-notes" class="relative border-s border-gray-200 dark:border-gray-700">
-                                            @foreach($carrier->carrierNotes as $carrierNote)
-                                                <li class="mb-5 ms-4">
-                                                    <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                                                    <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{{ \Carbon\Carbon::parse($carrierNote->created_at)->timezone('America/Detroit')->format('F jS @ g:i a') }}</time>
-                                                    <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{{ $carrierNote->note }}</p>
-                                                </li>
-                                            @endforeach
-                                        </ol>
-                                    @endif
+                                    <ol id="carrier-notes" class="relative border-s border-gray-200 dark:border-gray-700">
+                                        @if (isset($carrier) && ! $carrier->carrierNotes->isEmpty())
+                                                @foreach($carrier->carrierNotes as $carrierNote)
+                                                    <li class="mb-5 ms-4">
+                                                        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                                                        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{{ \Carbon\Carbon::parse($carrierNote->created_at)->timezone('America/Detroit')->format('F jS @ g:i a') }}</time>
+                                                        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{{ $carrierNote->note }}</p>
+                                                    </li>
+                                                @endforeach
+                                        @endif
+                                    </ol>
                                     <textarea id="notes" name="notes" rows="8" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your notes here"
                                     ></textarea>
                                 </div>
